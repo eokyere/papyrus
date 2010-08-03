0ae8a4f9eaa9e6a11b4e26cc637b17b3bc2386b3
Emmanuel Okyere <chief@hutspace.net>
2010-08-03 11:28:26 +0000

    converted parsed info into dicts

--- monitor_teledata.py
+++ monitor_teledata.py
@@ -42,8 +42,6 @@ class Scraping(object):
         html = re.sub('>[\s]+', '>', html) # remove whitespaces after closing tags
         return html 
 
-# download the page from http://192.168.10.1/ which redirects to main.cgi?mac_esn=0a003ed7cdf5
-# parse the page
 # update database. 
 # if down make a note to send uptime info next time it is up
 # if up, and there is a note to send a downtime email send
@@ -53,7 +51,8 @@ def main():
     print sxp.parse(url, parser)
     
 def parser(soup):
-    return (soup.find('span', id='RegisteredAP').string,
+    status = soup.find('span', id='RegisteredAP').string
+    return (status,
             parse_device_info(soup),
             parse_site_info(soup),
             parse_sm_stats(soup))
@@ -68,10 +67,13 @@ def parse_sm_stats(soup):
     return parse_section(soup, 'SectionSubscriberModemStats')
 
 def parse_section(soup, id):
-    soup = soup.find('div', id=id)
-    soup = soup.table.tbody.contents
-    xs = [(x.contents[0].string, x.contents[1].span.string) for x in soup]   
-    return xs  
+    soup = soup.find('div', id=id).table.tbody.contents
+    xs = [(x.contents[0].string, 
+           x.contents[1].span.string) for x in soup]
+    d = {}   
+    for k, v in xs:
+        d[k] = v
+    return d  
     
 if __name__ == "__main__":
     main()
